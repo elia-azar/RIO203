4c1426718e7cb0725b087e83c5ea12b280e76eaa
Elia Azar <elia.azar@polytechnique.edu>
2021-01-12 10:57:39 +0100

    Smart street light implementation

---
+++ RPi/SmartStreetLight.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import _thread
+from StreetLight import StreetLight
+import PCF8591 as ADC
+import RPi.GPIO as GPIO
+import time
+
+DO = 17
+GPIO.setmode(GPIO.BCM)
+Destroy = [False]
+
+def setup():
+    global lux
+    lux = 255
+    ADC.setup(0x48)
+    GPIO.setup(DO, GPIO.IN)
+
+
+def loop():
+    while True:
+        lux = ADC.read(0)
+        time.sleep(0.2)
+
+def destroy():
+    Destroy[0] = True
+    GPIO.cleanup()
+
+class SmartStreetLight:
+
+    def __init__(self):
+        setup()
+        _thread.start_new_thread(loop, ())
+        self.light = StreetLight()
+        self.state = self.light.state
+
+        def run(self):
+            while not Destroy[0]:
+                if lux < 100:
+                    self.update_state("ON")
+                else:
+                    self.update_state("OFF")
+                time.sleep(10*60)
+
+        _thread.start_new_thread(run, (self,))
+
+    def update_state(self, new_state):
+        self.state = new_state
+        return self.light.update_state(new_state)
+    
+    def get_state(self):
+        return self.state
+    
+    def get_consumption(self):
+        return self.light.get_consumption()
+    
+    def kill(self):
+        destroy()
+        self.light.kill()
--- RPi/StreetLight.py
+++ RPi/StreetLight.py
@@ -1,26 +1,70 @@
 from datetime import datetime
+import RPi.GPIO as GPIO
+
+
+STATES = {
+    "ON": 0xFFFF,
+    "OFF": 0x0000, 
+    }
+
+def setup():
+    global pins
+    global p_R, p_G
+
+    pins = (11, 12)  # pins is a dict
+
+    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+    GPIO.setup(pins, GPIO.OUT)   # Set pins' mode is output
+    GPIO.output(pins, GPIO.LOW)  # Set pins to LOW(0V) to off led
+
+    p_R = GPIO.PWM(pins[0], 2000)  # set Frequece to 2KHz
+    p_G = GPIO.PWM(pins[1], 2000)
+
+    p_R.start(0)      # Initial duty Cycle = 0(leds off)
+    p_G.start(0)
+
+def map(x, in_min, in_max, out_min, out_max):
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def setColor(col):   # For example : col = 0x1122
+	R_val = col  >> 8
+	G_val = col & 0x00FF
+	
+	R_val = map(R_val, 0, 255, 0, 100)
+	G_val = map(G_val, 0, 255, 0, 100)
+	
+	p_R.ChangeDutyCycle(R_val)     # Change duty cycle
+	p_G.ChangeDutyCycle(G_val)
+
+def destroy():
+	p_R.stop()
+	p_G.stop()
+	GPIO.output(pins, GPIO.LOW)    # Turn off all leds
+	GPIO.cleanup()
+
 
 class StreetLight:
     def __init__(self):
+        setup()
         self.date = datetime.now().strftime("%H:%M:%S")
         self.state = "OFF"
+        setColor(STATES.get(self.state))
 
     def update_state(self, new_state):
         self.state = new_state
+        setColor(STATES.get(self.state))
+        self.date = datetime.now().strftime("%H:%M:%S")
         return "200"
     
-    def get_value(self):
-        if self.state == "OFF":
-            return "Object is OFF"
-
-        # TODO implement function to get value from sensor node
-        value = 10
-        return value
+    def get_state(self):
+        return self.state
     
     def get_consumption(self):
-        if self.state == "OFF":
+        if self.state == "ON":
+            return 73
+        else:
             return 0
-
-        # TODO implement function to get the consumption of the node
-        consumption = 10
-        return consumption
+    
+    def kill(self):
+        destroy()
+        self.state = "OFF"
