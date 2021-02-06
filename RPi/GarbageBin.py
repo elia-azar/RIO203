a641796cda7c9c6e36b775f51b5ff390c28e9ae7
Elia Azar <elia.azar@polytechnique.edu>
2021-02-07 00:40:34 +0100

    Modified the garbage bin function

--- RPi/GarbageBin.py
+++ RPi/GarbageBin.py
@@ -34,27 +34,15 @@ def distance():
     during = time2 - time1
     return during * 340 / 2 * 100
 
-def GET():
-    global emptyspace
-    bin_D = [200,120,30] # the dimensions are in cm
-    bin_V = bin_D[0] * bin_D[1] * bin_D[2] # the capacity is in cubic cm 
-    dis = distance()
-    #thres = (0.80 * bin_V) /1000
-    emptyspace = bin_D[0] * bin_D[2] * dis / 1000000 # 1cc = 1ml 
-    if (emptyspace < bin_V):
-            print(emptyspace, "mL left,the bin is full")
-            verdict = str(emptyspace)
-            return verdict
-
 def loop():
-    while True: 
-        GET()
-        time.sleep(10)
-
+    global emptyspace
+    emptyspace = distance()
+    while True:
+        emptyspace = distance()
+        time.sleep(60)
 
 def destroy():
-    # A revoir
-    GPIO.cleanup() # Release resource
+    GPIO.cleanup()
 
 class GarbageBin:
     def __init__(self):
@@ -76,9 +64,13 @@ class GarbageBin:
         return emptyspace
 
     def update_state(self): # the states were defined earlier in the code. 
-        if emptyspace < threshold:  
+        if emptyspace < threshold:
+            if self.state == states[1]:
+                print("WARNING: Garbage is Full")  
             self.state = states[0]
         else:
+            if self.state == states[0]:
+                print("INFO: Garbage has been emptied") 
             self.state = states[1]
     
     def get_state(self):
